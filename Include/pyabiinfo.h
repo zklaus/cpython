334f96e98153d6027cb3c79ad58e23a5d69297cc
Klaus Zimmermann <klaus.zimmermann@quansight.com>
2025-08-01 19:03:56 +0200

    Add sys.abi_info

---
+++ Include/pyabiinfo.h
@@ -0,0 +1,13 @@
+#ifndef Py_PYABIINFO_H
+#define Py_PYABIINFO_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+PyAPI_FUNC(PyObject *) PyAbiInfo_GetInfo(void);
+
+#ifdef __cplusplus
+}
+#endif
+#endif /* !Py_PYABIINFO_H */
